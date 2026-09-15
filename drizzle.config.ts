2f647c60ff63fdc3730534d8c10377e559726182
rudals227 <rudals227@gmail.com>
2026-09-15 16:02:59 +0900

    chore: drizzle DB 연동 및 env 설정

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local' });
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/server/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: process.env.DATABASE_URL!,
   },
 });
